a82cce3f7088f8fb5854f1426526a7217dfa1371
marco alvarado <Marco@marcos-MacBook-Pro.local>
2017-01-27 19:37:52 -0800

    front end done

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var PokemonList= [“Pikachu”, “Charizard”,”Mewtwo" ]
+var Pokemon= PokemonList[Math.floor(Math.random() * PokemonList.length()-1 );];
+console.log(Pokemon);
+var Guess= document.onKeyUp
+
+document.onkeyup(){
+
+
+
+
+
+}
